806fcad266628a711ec7e0079fb9f14882ae7195
Sophia Castillo <Sphcastillo@gmail.com>
2020-09-21 19:04:02 -0700

    setup routes, models, server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded( {extended: true} ));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./ruotes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
